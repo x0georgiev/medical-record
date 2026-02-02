9b8e616ee7c3dceb51407d2dbf799912d968ca4f
batman <teodorgeorgiev@proton.me>
2026-02-02 20:02:52 +0200

    Create initial project structure

---
+++ src/main/java/com/cscb869/medical_record/MedicalRecordApplication.java
@@ -0,0 +1,13 @@
+package com.cscb869.medical_record;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalRecordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalRecordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cscb869/medical_record/MedicalRecordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cscb869.medical_record;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalRecordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
